217593045c9f01cc2023f7d2fa0b87a4ffc7688a
ouadhi <BITL19@DESKTOP-33Q4LJJ>
2019-12-29 11:26:44 +0100

    contener visite

--- src/main/java/com/douane/controler/ConterVisiteContrioller.java
+++ src/main/java/com/douane/controler/ConterVisiteContrioller.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douane.entities.Contrevisite;
+import com.douane.entities.Message;
 import com.douane.repository.ConterVisiteRepository;
 //end  point annotation 
 @RestController
@@ -24,14 +25,25 @@ public class ConterVisiteContrioller {
 	@Autowired  
 	private ConterVisiteRepository repository  ;  
 	
+	private String title  =  "Contenervisite" ; 
+	private Message<Contrevisite> message  = new Message<Contrevisite>()  ; 
+	
 	
 	/*  find all data  when flag  = true 
 	 *   data  not  consumed
 	 */
 	
 	@GetMapping(path = "/getdata")
-	public List<Contrevisite> findNotMarkedt ()  {
-		return repository.getDataNotMarked() ; 
+	public Message<Contrevisite> findNotMarkedt ()  {
+		Long start  =(Long) repository.findStartEndId().get(0).get("start") ;  
+		Long end = (Long)  repository.findStartEndId().get(0).get("end") ;  
+	    message.setId(this.title+"-"+start+"-"+end) ; 
+		message.setCount(repository.getCount());
+		message.setStart_id(start);
+		message.setEnd_id(end);
+		message.setDescription("contenaire visite ");
+		message.setContant( repository.getDataNotMarked());
+		return message;
 	}
 	
 	@GetMapping(path = "/getalldata")
@@ -87,6 +99,24 @@ public class ConterVisiteContrioller {
 		} else {
 			System.out.println("Record not exists with the Id: " + id);
 		}
+	}
+	
+	@PostMapping(path = "/marked/{start}/{end}", produces = "application/json")
+	public void markedlist(@PathVariable(name="start") Long start   ,@PathVariable(name="end") Long end    ) {
+		try {
+			
+			System.out.println(start +" "+ end);
+			repository.setMareked(start, end);
+			System.out.println("Data has been marked successfully :");
+		} catch (Exception e) {
+			System.out.println("Record not exists");
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	@GetMapping(path = "/getcount")
+	public int  getcount ()  {
+		return repository.getCount()  ; 
 	}
 
 
--- src/main/java/com/douane/repository/ConterVisiteRepository.java
+++ src/main/java/com/douane/repository/ConterVisiteRepository.java
@@ -1,15 +1,30 @@
 package com.douane.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.douane.entities.Contrevisite;
 
 
 public interface ConterVisiteRepository  extends  JpaRepository<Contrevisite, Long>{
 	@Query("SELECT  c FROM  Contrevisite  c WHERE c.flag =  false ")
-	public List<Contrevisite>  getDataNotMarked () ; ; 
+	public List<Contrevisite>  getDataNotMarked () ; 
+	
+	@Modifying
+	@Transactional
+	@Query(value = "update Contrevisite  set  flag='t' where id between :start and  :end  ", nativeQuery = true)
+	public void setMareked(@Param("start") Long start ,@Param("end") Long end  ) ; 
+	
+	@Query("Select Count(*) from Manifest WHERE  flag  = false ")
+	public int getCount () ; 
+	
+	@Query("select new  map(min(id) as start ,  max(id) as end ) from  Contrevisite where flag='f' ")
+	public List<Map<String, Object>> findStartEndId() ; 
 
 }
