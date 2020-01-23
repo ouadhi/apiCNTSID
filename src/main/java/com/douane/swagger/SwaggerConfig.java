a6c00b5987127c61f5fdacf08ebf054f6a627483
ouadhi <BITL19@DESKTOP-33Q4LJJ>
2020-01-23 23:59:20 +0100

    add Swagger Dependecy and  configuration class

---
+++ src/main/java/com/douane/swagger/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.douane.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket  api() {
+		return  new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors
+						.basePackage("com.douane"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiEndPointsInfo()) ; 
+	}
+	
+	private ApiInfo apiEndPointsInfo() {
+
+        return new ApiInfoBuilder().title("REST API FOR ALGERIAN CUSTEMS SERVICE ")
+                .description("CNTSID  REST API")
+                .contact(new Contact("ALGERIAN CUSTMES", "www.douane.gov.dz", "ouadhi@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0-SNAPSHOT")
+                .build();
+    }
+
+}
